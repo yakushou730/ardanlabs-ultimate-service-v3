65d593a1743ef2102be44ca15df14af98d37d921
Shou Tseng <yakushou730@gmail.com>
2022-05-14 19:03:01 +0800

    feat: basic notify setup

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	build = "develop"
+)
 
 func main() {
-	fmt.Println("hello world")
+	log.Println("starting service", build)
+	defer log.Println("service ended")
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+	<-shutdown
+
+	log.Println("stopping service")
 }
