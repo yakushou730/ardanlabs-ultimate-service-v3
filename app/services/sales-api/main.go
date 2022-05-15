c47bc414b493c9e2dcb48fac5c347ca7d1b23843
Shou Tseng <yakushou730@gmail.com>
2022-05-15 13:44:54 +0800

    feat: adjust project layout

--- app/services/sales-api/main.go
+++ app/services/sales-api/main.go
@@ -1,36 +1,119 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"go.uber.org/automaxprocs/maxprocs"
-	"log"
 	"os"
 	"os/signal"
 	"runtime"
 	"syscall"
+	"time"
+
+	"github.com/ardanlabs/conf"
+	"go.uber.org/automaxprocs/maxprocs"
 
 	_ "go.uber.org/automaxprocs"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
+
+/*
+Need to figure out timeouts for http service
+*/
 
 var (
 	build = "develop"
 )
 
 func main() {
-	// Set the correct number of threads for the service
-	// based on what is available either by the machine or quotas
-	if _, err := maxprocs.Set(); err != nil {
-		fmt.Printf("maxprocs: %s\n", err)
+	// Construct the application logger
+	log, err := initLogger("SALES-API")
+	if err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
+	defer log.Sync()
 
-	g := runtime.GOMAXPROCS(0)
-	log.Printf("Hello Shou, starting service build[%s] CPU[%d]", build, g)
-	defer log.Println("service ended")
+	// Perform the startup and shutdown sequence
+	if err := run(log); err != nil {
+		log.Errorw("startup", "ERROR", err)
+		os.Exit(1)
+	}
+}
+
+func run(log *zap.SugaredLogger) error {
+	// ==================================
+	// GOMAXPROCS
+
+	// Set the correct numbers of threads for the service
+	// based on what is available either by the machine or quotas.
+	if _, err := maxprocs.Set(); err != nil {
+		return fmt.Errorf("maxprocs: %w", err)
+	}
+	log.Infow("startup", "GOMAXPROCS", runtime.GOMAXPROCS(0))
+
+	// ==================================
+	// Configuration
+	cfg := struct {
+		conf.Version
+		Web struct {
+			APIHost         string        `conf:"default:0.0.0.0:3000"`
+			DebugHost       string        `conf:"default:0.0.0.0:4000"`
+			ReadTimeout     time.Duration `conf:"default:5s"`
+			WriteTimeout    time.Duration `conf:"default:10s"`
+			IdleTimeout     time.Duration `conf:"default:120s"`
+			ShutdownTimeout time.Duration `conf:"default:20s"`
+		}
+	}{
+		Version: conf.Version{
+			SVN:  build,
+			Desc: "copyright 2022 for yakushou730",
+		},
+	}
+
+	const prefix = "SALES"
+	help, err := conf.ParseOSArgs(prefix, &cfg)
+	if err != nil {
+		if errors.Is(err, conf.ErrHelpWanted) {
+			fmt.Println(help)
+			return nil
+		}
+		return fmt.Errorf("parsing config: %w", err)
+	}
+
+	// ==================================
+	// App Starting
+	log.Infow("starting service", "version", build)
+	defer log.Infow("shutdown complete")
+
+	out, err := conf.String(&cfg)
+	if err != nil {
+		return fmt.Errorf("generating config for output: %w", err)
+	}
+	log.Infow("startup", "config", out)
+
+	// ==================================
 
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
 	<-shutdown
 
-	log.Println("stopping service")
+	return nil
+}
+
+func initLogger(service string) (*zap.SugaredLogger, error) {
+	config := zap.NewProductionConfig()
+	config.OutputPaths = []string{"stdout"}
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.DisableStacktrace = true
+	config.InitialFields = map[string]interface{}{
+		"service": service,
+	}
+
+	log, err := config.Build()
+	if err != nil {
+		return nil, err
+	}
+
+	return log.Sugar(), nil
 }
