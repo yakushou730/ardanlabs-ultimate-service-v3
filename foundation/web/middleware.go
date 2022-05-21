96e6848a8f083e0f6a744e6c00751730c594855a
Shou Tseng <yakushou730@gmail.com>
2022-05-21 14:35:28 +0800

    feat: sending responses

--- foundation/web/middleware.go
+++ foundation/web/middleware.go
@@ -9,6 +9,9 @@ type Middleware func(Handler) Handler
 // handler. The middlewares' handlers will be executed by requests in the order
 // they are provided
 func wrapMiddleware(mw []Middleware, handler Handler) Handler {
+	if len(mw) == 0 {
+		return handler
+	}
 
 	// Loop backwards through the middleware invoking each one. Replace the
 	// handler with the new wrapped handler. Looping backwards ensures that the
