cd15bec5f71f773e50b3e2ed411d9e98a69361e5
Shou Tseng <yakushou730@gmail.com>
2022-06-03 08:21:38 +0800

    feat: implementation of an in-memory key store

---
+++ foundation/keystore/keystore.go
@@ -0,0 +1,130 @@
+// Package keystore implements the auth.KeyStore interface. This implements
+// an in-memory keystore for JWT support
+package keystore
+
+import (
+	"crypto/rsa"
+	"errors"
+	"fmt"
+	"github.com/golang-jwt/jwt/v4"
+	"io"
+	"io/fs"
+	"path"
+	"strings"
+	"sync"
+)
+
+// KeyStore represents an in memory store implementation of the
+// KeyStorer interface for use with teh auth package
+type KeyStore struct {
+	mu    sync.RWMutex
+	store map[string]*rsa.PrivateKey
+}
+
+// New constructs an empty KeyStore ready for use
+func New() *KeyStore {
+	return &KeyStore{
+		store: make(map[string]*rsa.PrivateKey),
+	}
+}
+
+// NewMap constructs a KeyStore with an initial set of keys
+func NewMap(store map[string]*rsa.PrivateKey) *KeyStore {
+	return &KeyStore{
+		store: store,
+	}
+}
+
+// NewFS constructs a KeyStore based on a set of PEM files rooted inside
+// of a directory. The name of each PEM file will be used as the key id
+// Example: keystore.NewFS(os.DirFS("/zarf/keys"))
+// Example: /zarf/keys/xxxx.pem
+func NewFS(fsys fs.FS) (*KeyStore, error) {
+	ks := KeyStore{
+		store: make(map[string]*rsa.PrivateKey),
+	}
+
+	fn := func(fileName string, dirEntry fs.DirEntry, err error) error {
+		if err != nil {
+			return fmt.Errorf("walkdir failuree: %w", err)
+		}
+
+		if dirEntry.IsDir() {
+			return nil
+		}
+
+		if path.Ext(fileName) != ".pem" {
+			return nil
+		}
+
+		file, err := fsys.Open(fileName)
+		if err != nil {
+			return fmt.Errorf("opening key file: %w", err)
+		}
+		defer file.Close()
+
+		// limit PEM file size to 1 megabyte. This should be reasonable for
+		// almost any PEM file and prevents shenanigans like linking the file
+		// to /dev/random or something like that
+		privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+		if err != nil {
+			return fmt.Errorf("reading auth private key: %w", err)
+		}
+
+		privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+		if err != nil {
+			return fmt.Errorf("parsing auth private key: %w", err)
+		}
+
+		ks.store[strings.TrimSuffix(dirEntry.Name(), ".pem")] = privateKey
+		return nil
+	}
+
+	if err := fs.WalkDir(fsys, ".", fn); err != nil {
+		return nil, fmt.Errorf("walking directory: %w", err)
+	}
+
+	return &ks, nil
+}
+
+// Add adds a private key and combination kid to the store
+func (ks *KeyStore) Add(privateKey *rsa.PrivateKey, kid string) {
+	ks.mu.Lock()
+	defer ks.mu.Unlock()
+
+	ks.store[kid] = privateKey
+}
+
+// Remove removes a private key and combination kid to the store
+func (ks *KeyStore) Remove(kid string) {
+	ks.mu.Lock()
+	defer ks.mu.Unlock()
+
+	delete(ks.store, kid)
+}
+
+// PrivateKey searches the key store for a given kid and returns
+// the private key
+func (ks *KeyStore) PrivateKey(kid string) (*rsa.PrivateKey, error) {
+	ks.mu.Lock()
+	defer ks.mu.Unlock()
+
+	privateKey, found := ks.store[kid]
+	if !found {
+		return nil, errors.New("kid lookup failed")
+	}
+	return privateKey, nil
+}
+
+// PublicKey searches the key store for a given kid and returns
+// the public key
+func (ks *KeyStore) PublicKey(kid string) (*rsa.PublicKey, error) {
+	ks.mu.Lock()
+	defer ks.mu.Unlock()
+
+	privateKey, found := ks.store[kid]
+	if !found {
+		return nil, errors.New("kid lookup failed")
+	}
+	return &privateKey.PublicKey, nil
+}
